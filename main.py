d90b74fb88e737c6b73147e95aa7795c6f95c27f
Farhan2520 <148778564+Farhan2520@users.noreply.github.com>
2026-05-28 11:59:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import firebase_admin
+from firebase_admin import credentials, db
+import json
+import os
 
-BOT_TOKEN = "8816672349:AAG41gLQ4DJEz4rwBV-onPJPgQgncRpQqfk"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+firebase_key = json.loads(os.getenv("FIREBASE_KEY"))
+
+cred = credentials.Certificate(firebase_key)
+
+firebase_admin.initialize_app(cred, {
+'databaseURL': 'https://seatswap-a96ec-default-rtdb.firebaseio.com/'
+})
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello Farhan 🔥 SeatSwap Bot Working!")
+
+```
+user_id = str(update.effective_user.id)
+
+ref = db.reference(f"users/{user_id}")
+
+ref.set({
+    "name": update.effective_user.first_name,
+    "points": 0,
+    "rating": 5
+})
+
+await update.message.reply_text(
+    "Welcome to SeatSwap 🚆\nYour profile has been created!"
+)
+```
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
