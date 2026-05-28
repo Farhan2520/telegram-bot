465141687d3b1bec0db8165558e22374ee7abde9
Farhan2520 <ir.farhan18@gmail.com>
2026-05-28 09:34:53 +0530

    First upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello Farhan")
+
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
+app.run_polling()
