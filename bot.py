8e3815f9fd2755ef082e2f4aef9c6cc6f1ae9d61
Farhan2520 <148778564+Farhan2520@users.noreply.github.com>
2026-05-28 11:19:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler
 BOT_TOKEN = "8816672349:AAG41gLQ4DJEz4rwBV-onPJPgQgncRpQqfk"
 
 def start(update, context):
-update.message.reply_text("Hello Farhan 🔥 SeatSwap Bot Working!")
+    update.message.reply_text("Hello Farhan 🔥 SeatSwap Bot Working!")
 
 updater = Updater(BOT_TOKEN, use_context=True)
 
