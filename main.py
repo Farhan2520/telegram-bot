ea02019acfefa45c1e51c887bbc6793946cdccef
Farhan2520 <148778564+Farhan2520@users.noreply.github.com>
2026-05-28 12:44:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "8816672349:AAG41gLQ4DJEz4rwBV-onPJPgQgncRpQqfk"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("SeatSwap Bot Working 🔥")
