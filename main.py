cf95e9d3a554d2dd9298d6bbeb371fa339612fa2
Farhan2520 <148778564+Farhan2520@users.noreply.github.com>
2026-05-28 11:36:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
 BOT_TOKEN = "8816672349:AAG41gLQ4DJEz4rwBV-onPJPgQgncRpQqfk"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello Farhan 🔥 Bot Working!")
+await update.message.reply_text("Hello Farhan 🔥 SeatSwap Bot Working!")
 
-app = ApplicationBuilder().token(TOKEN).build()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
 
 app.run_polling()
